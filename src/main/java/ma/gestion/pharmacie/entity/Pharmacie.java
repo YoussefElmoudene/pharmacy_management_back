3a43c2419d0a0df4d43363e9722845988e43fc1c
YoussefElmoudene <elmoudene.ysf@gmail.com>
2023-03-18 17:40:58 +0100

    first commit

--- src/main/java/ma/gestion/pharmacie/entity/Pharmacie.java
+++ src/main/java/ma/gestion/pharmacie/entity/Pharmacie.java
@@ -3,11 +3,15 @@ package ma.gestion.pharmacie.entity;
 import javax.persistence.*;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class Pharmacie {
 	
 	@Id
